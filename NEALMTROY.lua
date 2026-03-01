8cd4157b8dd278dead693393fe6526a3284e20b0
nealmtroy <51311144+nealmtroy@users.noreply.github.com>
2026-03-01 14:26:10 +0700

    Update NEALMTROY.lua

--- NEALMTROY.lua
+++ NEALMTROY.lua
@@ -1,1 +1,12 @@
+local registeredMaps = {
+    [130594398886540] = "https://raw.githubusercontent.com/nealmtroy/nealmtroy/refs/heads/main/Games/GH.lua"
+}
 
+local currentPlaceId = game.PlaceId
+local scriptToLoad = registeredMaps[currentPlaceId]
+
+if scriptToLoad then
+    loadstring(game:HttpGet(scriptToLoad))()
+else
+    script:Destroy()
+end
