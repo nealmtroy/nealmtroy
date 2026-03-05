5f0efa34671fffdf0ed085f29b812f95db4c1310
NEALMTROY <51311144+nealmtroy@users.noreply.github.com>
2026-03-05 22:32:14 +0700

    Update NEALMTROY.lua

--- NEALMTROY.lua
+++ NEALMTROY.lua
@@ -1,5 +1,6 @@
 local registeredMaps = {
-    [130594398886540] = "https://raw.githubusercontent.com/nealmtroy/nealmtroy/refs/heads/main/Games/GH.lua"
+    [130594398886540] = "https://raw.githubusercontent.com/nealmtroy/nealmtroy/refs/heads/main/Games/GH.lua",
+    [119987266683883] = "https://raw.githubusercontent.com/nealmtroy/nealmtroy/refs/heads/main/Games/SLFB.lua"
 }
 
 local currentPlaceId = game.PlaceId
