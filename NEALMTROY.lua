1224977cbb3baaed51d7775317d7a4f967cfc47a
NEALMTROY <51311144+nealmtroy@users.noreply.github.com>
2026-03-07 18:57:03 +0700

    Update NEALMTROY.lua

--- NEALMTROY.lua
+++ NEALMTROY.lua
@@ -1,10 +1,13 @@
 local registeredMaps = {
     [130594398886540] = "https://raw.githubusercontent.com/nealmtroy/nealmtroy/refs/heads/main/Games/GH.lua",
     [119987266683883] = "https://raw.githubusercontent.com/nealmtroy/nealmtroy/refs/heads/main/Games/SLFB.lua",
-    [114640202062357] = "https://raw.githubusercontent.com/nealmtroy/nealmtroy/refs/heads/main/Games/SOFB.lua"
+    [114640202062357] = "https://raw.githubusercontent.com/nealmtroy/nealmtroy/refs/heads/main/Games/SOFB.lua",
+    [139299356663913] = "https://raw.githubusercontent.com/nealmtroy/nealmtroy/refs/heads/main/Games/FOB.lua"
 }
+
 local currentPlaceId = game.PlaceId
 local scriptToLoad = registeredMaps[currentPlaceId]
+
 if scriptToLoad then
     loadstring(game:HttpGet(scriptToLoad))()
 else
